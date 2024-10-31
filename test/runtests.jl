49de0f66a1d6c8a4d56aa015766bee7a9cbead05
Tommaso Stentella <stentell@molgen.mpg.de>
2024-10-31 14:23:40 +0100

    solved conflicts and updated julia lts

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,13 @@ using Test
 @testset "Stationary" begin
     TN = [250_000_000, 10_000]
     μ = 1e-8
-    rs = logrange(1,1e6,200)
+    rs = [1,1e6]
     y = map(x->hid(TN, μ, x), rs)
 end
 
 @testset "Bottleneck" begin
     TN = [250_000_000, 10_000, 2000, 2000, 5000, 10_000]
     μ = 1e-8
-    rs = logrange(1,1e6,200)
+    rs = [1,1e6]
     y = map(x->hid(TN, μ, x), rs)
 end
