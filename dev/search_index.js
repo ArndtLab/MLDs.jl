a3fefbfcb783d670c86e9e4a7a2e4758ac75bd7f
Documenter.jl <documenter@juliadocs.github.io>
2025-06-03 09:58:44 +0000

    build based on 7f9ec15

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MLDs","category":"page"},{"location":"#MLDs","page":"Home","title":"MLDs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MLDs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MLDs]","category":"page"}]
+[{"location":"#MLDs","page":"Home","title":"MLDs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MLDs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
